80b054a4c9fcea7184300fb3c71ae97cf8e1df20
Haotian Zheng <justzht@gmail.com>
2022-04-16 00:47:39 -0700

    Initial Commit

---
+++ ShaderNodeEditor/ShaderNodeEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaderNodeEditorApp.swift
+//  ShaderNodeEditor
+//
+//  Created by fincher on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ShaderNodeEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
