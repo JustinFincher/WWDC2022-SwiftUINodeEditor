2799ea598d9e5adda6678d7e07024ab0081e98b2
JustZht <justzht@gmail.com>
2022-04-17 17:26:54 -0700

    feat: make playground file compatible with iPad client

--- Playgrounds/Shader.swiftpm/Package.swift
+++ Playgrounds/Shader.swiftpm/Package.swift
@@ -8,13 +8,13 @@ import PackageDescription
 import AppleProductTypes
 
 let package = Package(
-    name: "My App",
+    name: "Shader Node Editor",
     platforms: [
         .iOS("15.2")
     ],
     products: [
         .iOSApplication(
-            name: "My App",
+            name: "Shader Node Editor",
             targets: ["AppModule"],
             displayVersion: "1.0",
             bundleVersion: "1",
