d4dcff7f6f514b5f29d282eb8e6f37452cdcacf2
Haotian Zheng <justzht@gmail.com>
2022-04-18 01:44:46 -0700

    feat: add popover

--- ShaderNodeEditor/View/NodeCanvasNavigationView.swift
+++ ShaderNodeEditor/View/NodeCanvasNavigationView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct NodeCanvasNavigationView: View {
+    @State var isShowingPopover = false
     var body: some View {
         NavigationView {
             NodeCanvasView()
                 .navigationTitle("Editor")
                 .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            isShowingPopover = true
+                        }, label: {
+                            Image(systemName: "plus")
+                        })
+                        .popover(isPresented: $isShowingPopover) {
+                                    Text("Popover Content")
+                                        .padding()
+                                }
+                    }
+                }
         }.navigationViewStyle(.stack)
     }
 }
