615a9cf20ca54beb7afed9062cd7b63302d3cd68
Haotian Zheng <justzht@gmail.com>
2022-04-21 06:48:11 -0700

    feat: re-structure

--- generate_playground.sh
+++ generate_playground.sh
@@ -1,5 +1,5 @@
-cd Playgrounds/ScriptNode.swiftpm
+cd Playgrounds/ScriptNode.swiftpm/App
 ls -1 | egrep -v "^(Assets.xcassets|Package.swift)$" # | xargs rm -r
 ls -1 | egrep -v "^(Assets.xcassets|Package.swift)$" | xargs rm -r
-rsync -av --exclude='Assets.xcassets' --exclude='NodeEditor.entitlements' --exclude='Preview Content' ../../NodeEditor/ ./
-cd ../..
+rsync -av --exclude='Assets.xcassets' --exclude='NodeEditor.entitlements' --exclude='Preview Content' ../../../NodeEditor/ ./
+cd ../../..
