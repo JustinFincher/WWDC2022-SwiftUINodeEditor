eec5052cd9bb3bc1355b62516486dc91f264a4d5
Haotian Zheng <justzht@gmail.com>
2022-04-21 06:43:00 -0700

    fix: delete old node port data file

--- NodeEditor/Data/NodePortData.swift
+++ NodeEditor/Data/NodePortData.swift
@@ -8,11 +8,14 @@
 import Foundation
 import UIKit
 import Combine
+import SwiftUI
 
 protocol NodePortProtocol : ObservableObject, Identifiable, Hashable {
     func canConnect() -> Bool
-    func canConnectTo(anotherPort : NodeDataPortData)
-    func connectTo(anotherPort : NodeDataPortData)
+    func canConnectTo(anotherPort : Self) -> Bool
+    func connectTo(anotherPort :Self)
+    func icon() -> Image
+    func color() -> Color
 }
 
 enum NodePortDirection : String {
@@ -20,9 +23,33 @@ enum NodePortDirection : String {
     case output
 }
 
-
-class NodeDataPortData : ObservableObject, Identifiable, Equatable, Hashable {
-    static func == (lhs: NodeDataPortData, rhs: NodeDataPortData) -> Bool {
+class NodePortData : NodePortProtocol {
+    
+    
+    @Published var portID : Int
+    @Published var direction : NodePortDirection = .input
+    @Published var name = ""
+    @Published var canvasRect = CGRect.zero
+    @Published var connections : [NodePortConnectionData] = [] {
+        willSet {
+            newValue.forEach({ connection in
+                connection.objectWillChange.assign(to: &$childWillChange)
+            })
+        }
+    }
+    @Published private var childWillChange: Void = ()
+    
+    init(portID: Int, direction: NodePortDirection) {
+        self.portID = portID
+        self.direction = direction
+    }
+    
+    convenience init(portID: Int, name: String, direction: NodePortDirection) {
+        self.init(portID: portID, direction: direction)
+        self.name = name
+    }
+       
+    static func == (lhs: NodePortData, rhs: NodePortData) -> Bool {
         return lhs.portID == rhs.portID
         && lhs.direction == rhs.direction
         && lhs.name == rhs.name
@@ -38,64 +65,126 @@ class NodeDataPortData : ObservableObject, Identifiable, Equatable, Hashable {
         hasher.combine(connections)
     }
     
-    @Published var portID : Int
-    @Published var direction : NodePortDirection = .input
-    @Published var name = ""
-    @Published var canvasRect = CGRect.zero
-    @Published var connections : [NodePortConnectionData] = [] {
-        willSet {
-            newValue.forEach({ connection in
-                connection.objectWillChange.assign(to: &$childWillChange)
-            })
-        }
+    func icon() -> Image {
+        Image(systemName: "circlebadge.fill")
     }
     
-    @Published private var childWillChange: Void = ()
-    
-    init(portID: Int, direction: NodePortDirection) {
-        self.portID = portID
-        self.direction = direction
-    }
-    
-    convenience init(portID: Int, name: String, direction: NodePortDirection) {
-        self.init(portID: portID, direction: direction)
-        self.name = name
+    func color() -> Color {
+        Color.black
     }
     
     func canConnect() -> Bool {
-        if (self.direction == .input && !self.connections.isEmpty) {
+        false
+    }
+    
+    func canConnectTo(anotherPort: NodePortData) -> Bool {
+        false
+    }
+    
+    func connectTo(anotherPort: NodePortData) {
+        
+    }
+}
+
+// Control flow
+class NodeControlPortData : NodePortData {
+    
+    override func icon() -> Image {
+        return Image(systemName: "arrowtriangle.forward.fill")
+    }
+    
+    override func color() -> Color {
+        Color.blue
+    }
+    
+    override func canConnect() -> Bool {
+        if (self.direction == .output && !self.connections.isEmpty) {
             return false
         }
         return true
     }
     
-    func canConnectTo(anotherPort : NodeDataPortData) -> Bool {
-        // input node can only connect at most 1
-        if (self.direction == .input && !self.connections.isEmpty) {
-            return false
+    override func canConnectTo(anotherPort: NodePortData) -> Bool {
+        if let anotherPort = anotherPort as? NodeControlPortData {
+            // cannot connect same direction
+            if (self.direction == anotherPort.direction) {
+                return false
+            }
+            // TODO: cannot connect port on same node
+            return true
         }
-        // cannot connect same direction
-        if (self.direction == anotherPort.direction) {
-            return false
-        }
-        // cannot connect port on same node
-        return true
+        return false
     }
     
-    func connectTo(anotherPort : NodeDataPortData) {
+    override func connectTo(anotherPort: NodePortData) {
         if (!canConnectTo(anotherPort: anotherPort)) {
             return
         }
-        let nodePortConnection = NodePortConnectionData()
-        if direction == .output {
-            nodePortConnection.startPort = self
-            nodePortConnection.endPort = anotherPort
-        } else {
-            nodePortConnection.endPort = self
-            nodePortConnection.startPort = anotherPort
+        if let anotherPort = anotherPort as? NodeControlPortData {
+            let nodePortConnection = NodePortConnectionData()
+            if direction == .output {
+                nodePortConnection.startPort = self
+                nodePortConnection.endPort = anotherPort
+            } else {
+                nodePortConnection.endPort = self
+                nodePortConnection.startPort = anotherPort
+            }
+            self.connections.append(nodePortConnection)
+            anotherPort.connections.append(nodePortConnection)
         }
-        self.connections.append(nodePortConnection)
-        anotherPort.connections.append(nodePortConnection)
-        
+    }
+}
+
+
+// Data flow
+class NodeDataPortData : NodePortData {
+    
+    override func icon() -> Image {
+        return Image(systemName: "circlebadge.fill")
+    }
+    
+    override func color() -> Color {
+        Color.green
+    }
+    
+    override func canConnect() -> Bool {
+        if (self.direction == .input && !self.connections.isEmpty) {
+            return false
+        }
+        return true
+    }
+    
+    override func canConnectTo(anotherPort: NodePortData) -> Bool {
+        if let anotherPort = anotherPort as? NodeDataPortData {
+            // input node can only connect at most 1
+            if (self.direction == .input && !self.connections.isEmpty) {
+                return false
+            }
+            // cannot connect same direction
+            if (self.direction == anotherPort.direction) {
+                return false
+            }
+            // TODO: cannot connect port on same node
+            return true
+        }
+        return false
+    }
+    
+    override func connectTo(anotherPort: NodePortData) {
+        if (!canConnectTo(anotherPort: anotherPort)) {
+            return
+        }
+        if let anotherPort = anotherPort as? NodeDataPortData {
+            let nodePortConnection = NodePortConnectionData()
+            if direction == .output {
+                nodePortConnection.startPort = self
+                nodePortConnection.endPort = anotherPort
+            } else {
+                nodePortConnection.endPort = self
+                nodePortConnection.startPort = anotherPort
+            }
+            self.connections.append(nodePortConnection)
+            anotherPort.connections.append(nodePortConnection)
+        }
     }
 }
